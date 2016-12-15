c4e4e38ccc591bac8ecd6e4f4f71aed113d73116
VictiniX888 <victiniy@gmail.com>
2016-12-15 14:39:01 +0800

    Day 15

---
+++ AdventOfCode-Java/src/aoc2016/day15/Day15Part1.java
@@ -0,0 +1,48 @@
+package aoc2016.day15;
+
+import java.util.Scanner;
+
+public class Day15Part1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input: "); // split by ':'
+        String input = scanner.nextLine();
+        String[] splitInput = input.split(":");
+
+        int[][] positions = new int[splitInput.length][2];
+        int time = 0;
+
+        for (int i = 0; i < splitInput.length; i++) {
+            String[] sSplit = splitInput[i].split("\\s|\\.");
+            positions[i][0] = Integer.parseInt(sSplit[3]);
+            positions[i][1] = (Integer.parseInt(sSplit[11]) + i + 1) % positions[i][0];
+        }
+
+        while(!canPress(positions)) {
+            for (int i = 0; i < positions.length; i++) {
+                positions[i][1]++;
+                positions[i][1] %= positions[i][0];
+            }
+
+            time++;
+        }
+
+        System.out.println(time);
+    }
+
+    private static boolean canPress(int[][] pos) {
+
+        boolean legit = true;
+
+        for (int[] p : pos) {
+            if (p[1] != 0) {
+                legit = false;
+                break;
+            }
+        }
+
+        return legit;
+    }
+}
