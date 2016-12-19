cf23368370e9e6afcaeb39670fe333270b8ac510
VictiniX888 <victiniy@gmail.com>
2016-12-19 17:34:10 +0800

    2016 Day 18

---
+++ AdventOfCode-Java/src/aoc2016/day18/Day18Part2.java
@@ -0,0 +1,51 @@
+package aoc2016.day18;
+
+import java.util.Scanner;
+
+public class Day18Part2 {
+
+    private static final int ROWS = 400000;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input: ");
+        String input = scanner.nextLine();
+
+        boolean[][] tiles = new boolean[ROWS][input.length()];   // false = safe ; true = trap
+        int safeCount = 0;
+
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles[i].length; j++) {
+                if(i == 0) {
+                    char c = input.charAt(j);
+                    if(c == '^') {
+                        tiles[i][j] = true;
+                    }
+                    else {
+                        safeCount++;
+                    }
+                }
+                else {
+                    boolean left = false, centre = false, right = false;
+                    if(j != 0) {
+                        left = tiles[i-1][j-1];
+                    }
+                    centre = tiles[i-1][j];
+                    if(j != tiles[i-1].length - 1) {
+                        right = tiles[i-1][j+1];
+                    }
+
+                    if((left && centre && !right) || (!left && centre && right) || (left && !centre && !right) || (!left && !centre && right)) {
+                        tiles[i][j] = true;
+                    }
+                    else {
+                        safeCount++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(safeCount);
+    }
+}
